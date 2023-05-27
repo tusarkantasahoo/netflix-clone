b2d0757f31d3cb2f038f3cb365a09bae2c5407ae
tusarkantasahoo <30628537+tusarkantasahoo@users.noreply.github.com>
2023-05-28 01:19:14 +0530

    resolved the issues

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -14,8 +14,8 @@ const Auth = () => {
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
             <div className="flex flex-col gap-4">
-            <Input label="Email" onChange={(e)=>{setEmail(e.target.value)}} id="email" type="email" value={email} />
-            <Input label="Password" onChange={(e)=>{setEmail(e.target.value)}} id="password" type="password" value=""/>
+            <Input label="Email" onChange={(e:any)=>{setEmail(e.target.value)}} id="email" type="email" value={email} />
+            <Input label="Password" onChange={(e:any)=>{setEmail(e.target.value)}} id="password" type="password" value=""/>
             <button className="bg-red-600 py-3 text-white rounded-md w-full font-600 mt-6 hover:bg-red-700">Login</button>
             <p className="text-neutral-500 text-lg mt-10">First time using Netflix? <span className="text-white text-bold hover:underline cursor-pointer">Signup</span></p>
             </div>
